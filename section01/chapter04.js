340c1d9881f398da4479491dff3ee6adadefe197
유종화 <sola201@naver.com>
2025-09-18 10:49:17 +0900

    Add JavaScript basic

---
+++ section01/chapter04.js
@@ -0,0 +1,26 @@
+console.log('chapter 04');
+
+// 1. 변수
+let age = 27;
+age = 28;
+
+// 2. 상수
+const name = 'Mike';
+// name = 'Jane'; // Error
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let user1;
+// let 1user; // Error
+
+// 3-3. 예악어를 사용할 수 없다.
+// let if; // Error
+
+// 4. 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
+
